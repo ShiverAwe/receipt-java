c14d049d802e340f53eb576a5df11732e4006290
Vlad <vladislavnesterov_1@icloud.com>
2019-08-09 14:01:41 +0300

    First commit

--- src/main/java/com/github/shiverawe/controller/Merchant_Inform.java
+++ src/main/java/com/github/shiverawe/controller/Merchant_Inform.java
@@ -1,16 +1,14 @@
 package com.github.shiverawe.controller;
+
 import json;
 import urllib;
-  from typing import Dict, List;
+  from typing import Dict,List;
   from urllib.error import URLError
 
   from src.dao.models import Merchant
 
-
-
 public class Merchant_Inform {
-
-
+    
   void get_merchant_from_prima_inform_or_none(String inn) {
     try:
     Object response_json = urllib.request.urlopen("https://www.prima-inform.ru/utils/search?query=" + inn).read();
