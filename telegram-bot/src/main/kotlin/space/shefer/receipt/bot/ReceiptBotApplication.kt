81fcf543fd8ef52d355cb193385cdeabfd84e2ca
Vladimir Shefer <quameu@gmail.com>
2020-03-01 21:45:46 +0300

    Rename Main.kt to ReceiptBotApplication.kt pull to outer package

--- telegram-bot/src/main/kotlin/space/shefer/receipt/bot/ReceiptBotApplication.kt
+++ telegram-bot/src/main/kotlin/space/shefer/receipt/bot/ReceiptBotApplication.kt
@@ -1,10 +1,11 @@
-package space.shefer.receipt.bot.telegram
+package space.shefer.receipt.bot
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
 import org.telegram.telegrambots.ApiContextInitializer
 import org.telegram.telegrambots.meta.TelegramBotsApi
+import space.shefer.receipt.bot.telegram.ReceiptTelegramBot
 
 @SpringBootApplication
 class ReceiptBotApplication {
