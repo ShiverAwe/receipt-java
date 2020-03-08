261fe0dddb79e955c52115f54bbaef4fb6d92822
Vladimir Shefer <quameu@gmail.com>
2020-03-08 19:16:35 +0300

    telegram-bot: Add support for HTTP-proxy

--- telegram-bot/src/main/kotlin/space/shefer/receipt/bot/telegram/ReceiptTelegramBot.kt
+++ telegram-bot/src/main/kotlin/space/shefer/receipt/bot/telegram/ReceiptTelegramBot.kt
@@ -2,14 +2,22 @@ package space.shefer.receipt.bot.telegram
 
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import org.telegram.telegrambots.bots.DefaultBotOptions
 import org.telegram.telegrambots.bots.TelegramLongPollingBot
 import org.telegram.telegrambots.meta.api.objects.Update
 import space.shefer.receipt.bot.telegram.message_handlers.MessageHandler
 
 @Component
 class ReceiptTelegramBot(
-        private val messageHandlers: List<MessageHandler>
-) : TelegramLongPollingBot() {
+        private val messageHandlers: List<MessageHandler>,
+        private val proxyOptions: TelegramHttpProxyOptions
+) : TelegramLongPollingBot(DefaultBotOptions().also {
+    if (proxyOptions.enabled) {
+        it.proxyHost = proxyOptions.host!!
+        it.proxyPort = proxyOptions.port!!
+        it.proxyType = DefaultBotOptions.ProxyType.HTTP
+    }
+}) {
 
     @Value("\${bot.token}")
     lateinit var token: String
