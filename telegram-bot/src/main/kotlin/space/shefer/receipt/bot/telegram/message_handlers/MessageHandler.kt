ed34fdc0abe0aa3fa9236b75831107f041b76e35
Vladimir Shefer <quameu@gmail.com>
2020-03-09 10:22:47 +0300

    telegram-bot: Add javadoc for MessageHandler::handle

--- telegram-bot/src/main/kotlin/space/shefer/receipt/bot/telegram/message_handlers/MessageHandler.kt
+++ telegram-bot/src/main/kotlin/space/shefer/receipt/bot/telegram/message_handlers/MessageHandler.kt
@@ -5,6 +5,10 @@ import org.telegram.telegrambots.meta.api.objects.Update
 
 interface MessageHandler {
 
+    /**
+     * Any exception may be thrown from this method.
+     * Thrown exception is catched and ignored by call site.
+     */
     fun handle(bot: TelegramLongPollingBot, update: Update)
 
 }
