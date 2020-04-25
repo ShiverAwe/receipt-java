5ab0f0176bcf2efa10e3a922928bb5b98097bbcb
Vlad <vladislavnesterov_1@icloud.com>
2020-04-26 02:22:55 +0300

    added Receipts for IDLE

--- rest-api/src/main/java/space/shefer/receipt/rest/repository/ReceiptRepository.java
+++ rest-api/src/main/java/space/shefer/receipt/rest/repository/ReceiptRepository.java
@@ -1,12 +1,16 @@
 package space.shefer.receipt.rest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import space.shefer.receipt.rest.dto.ReportMetaFilter;
 import space.shefer.receipt.rest.entity.Receipt;
 
 import java.util.List;
 
 public interface ReceiptRepository extends JpaRepository<Receipt, Long>, ReceiptRepositoryCustom {
+
+  @Query("SELECT r FROM Receipt r WHERE status = 'IDLE'")
+  List<Receipt> findAllIdle();
 }
 
 interface ReceiptRepositoryCustom {
