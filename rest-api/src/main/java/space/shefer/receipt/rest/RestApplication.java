899c951912c26154e199031dfe45026626c910ef
Mikhail Yasnov <yasnov.mi@gmail.com>
2020-04-26 21:34:37 +0300

    Create ReceiptLoadJob that loads idle receipts from fns

--- rest-api/src/main/java/space/shefer/receipt/rest/RestApplication.java
+++ rest-api/src/main/java/space/shefer/receipt/rest/RestApplication.java
@@ -2,7 +2,11 @@ package space.shefer.receipt.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@ComponentScan("space.shefer.receipt.fnssdk")
+@EnableScheduling
 @SpringBootApplication
 public class RestApplication {
   public static void main(String[] args) {
