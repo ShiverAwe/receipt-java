7fb77d9363e9710aa2b29650f608a6a6b650de86
Vlad <vladislavnesterov_1@icloud.com>
2020-05-16 01:57:57 +0300

    RSJ-55 Refactor stream

--- rest-api/src/main/java/space/shefer/receipt/rest/service/MerchantLogoService.java
+++ rest-api/src/main/java/space/shefer/receipt/rest/service/MerchantLogoService.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Service;
 import space.shefer.receipt.rest.dto.MerchantSimpleName;
 
 import javax.annotation.Nullable;
-import java.util.NoSuchElementException;
 import java.util.stream.Stream;
 
 @Service
@@ -12,24 +11,10 @@ public class MerchantLogoService {
 
   @Nullable
   public String getUrlForImagePlace(String namePlace) {
-    String url;
-    // We must to use toAUpperCase method for namePlace because method contains not contain ignoreCase method.
-    try {
-      url = Stream.of(MerchantSimpleName.values())
-        .filter(merchant -> merchant.getValue().contains(namePlace.toUpperCase()))
-        .findFirst()
-        .map(MerchantSimpleName::getUrl).get();
-    }
-    catch (NoSuchElementException e) {
-      return null;
-    }
-
-    return url;
-  }
-
-  public static void main(String[] args) {
-    MerchantLogoService merchantLogoService = new MerchantLogoService();
-    System.out.println(merchantLogoService.getUrlForImagePlace("лента"));
+    return Stream.of(MerchantSimpleName.values())
+      .filter(merchant -> merchant.getValue().contains(namePlace.toUpperCase()))
+      .findFirst()
+      .map(MerchantSimpleName::getLogoUrl).orElse(null);
   }
 
 }
