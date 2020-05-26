58d0130b6f225cc43fa91da560465eac7841a5be
Vlad <vladislavnesterov_1@icloud.com>
2020-05-26 20:04:47 +0300

    RCJ-58 move MerchantSimpleName from rest.dto to rest.util

--- rest-api/src/main/java/space/shefer/receipt/rest/service/MerchantLogoService.java
+++ rest-api/src/main/java/space/shefer/receipt/rest/service/MerchantLogoService.java
@@ -1,7 +1,7 @@
 package space.shefer.receipt.rest.service;
 
 import org.springframework.stereotype.Service;
-import space.shefer.receipt.rest.module.MerchantSimpleName;
+import space.shefer.receipt.rest.util.MerchantSimpleName;
 
 import javax.annotation.Nullable;
 import java.util.stream.Stream;
