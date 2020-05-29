b88108e9d9f1b22422cfba2413d68a5bffad4fb8
vnesterov <48258305+VladNesterov@users.noreply.github.com>
2020-05-30 01:01:18 +0300

    RCJ-65 add @Param for HQL (#66)

--- rest-api/src/main/java/space/shefer/receipt/rest/RestApplication.java
+++ rest-api/src/main/java/space/shefer/receipt/rest/RestApplication.java
@@ -2,12 +2,14 @@ package space.shefer.receipt.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import space.shefer.receipt.fnssdk.config.EnableFnsSdk;
 
 @EnableFnsSdk
 @EnableScheduling
 @SpringBootApplication
+@EnableJpaAuditing
 public class RestApplication {
   public static void main(String[] args) {
     SpringApplication.run(RestApplication.class, args);
--- rest-api/src/main/java/space/shefer/receipt/rest/repository/UserProfileRepository.java
+++ rest-api/src/main/java/space/shefer/receipt/rest/repository/UserProfileRepository.java
@@ -2,12 +2,13 @@ package space.shefer.receipt.rest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import space.shefer.receipt.rest.entity.UserProfile;
 
 
 public interface UserProfileRepository extends JpaRepository<UserProfile, String> {
 
   @Query("SELECT up FROM UserProfile up WHERE up.phone = :phone")
-  UserProfile getByPhone(String phone);
+  UserProfile getByPhone(@Param("phone") String phone);
 
 }
