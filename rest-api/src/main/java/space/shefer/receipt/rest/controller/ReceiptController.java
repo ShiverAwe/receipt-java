02363ea54e788510082598603a69ca27c2d74dbc
Vladimir Shefer <quameu@gmail.com>
2020-05-30 19:49:42 +0300

    Merge ReceiptCreateController and ReceiptDeleteController into ReceiptController

--- rest-api/src/main/java/space/shefer/receipt/rest/controller/ReceiptController.java
+++ rest-api/src/main/java/space/shefer/receipt/rest/controller/ReceiptController.java
@@ -1,31 +1,27 @@
 package space.shefer.receipt.rest.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import space.shefer.receipt.platform.core.entity.Receipt;
 import space.shefer.receipt.platform.core.entity.UserProfile;
 import space.shefer.receipt.platform.core.service.UserProfileService;
 import space.shefer.receipt.rest.converters.ReceiptMetaConverter;
 import space.shefer.receipt.rest.dto.ReceiptCreateDto;
 import space.shefer.receipt.rest.dto.ReceiptMetaDto;
-import space.shefer.receipt.platform.core.entity.Receipt;
 import space.shefer.receipt.rest.service.ReceiptService;
 
 @RestController
-public class ReceiptCreateController {
+@RequiredArgsConstructor
+public class ReceiptController {
 
   private final ReceiptService receiptService;
   private final UserProfileService userProfileService;
-
-  @Autowired
-  public ReceiptCreateController(ReceiptService receiptService, UserProfileService userProfileService) {
-    this.receiptService = receiptService;
-    this.userProfileService = userProfileService;
-  }
 
   @RequestMapping(value = "/create", method = RequestMethod.POST)
   public ReceiptMetaDto create(@RequestBody ReceiptCreateDto query,
@@ -36,6 +32,11 @@ public class ReceiptCreateController {
     }
     Receipt receipt = receiptService.create(query, userProfile);
     return ReceiptMetaConverter.toDto(receipt);
+  }
+
+  @RequestMapping(value = "/delete", method = RequestMethod.DELETE)
+  public void delete(@RequestParam long id) {
+    receiptService.deleteReceipt(id);
   }
 
 }
