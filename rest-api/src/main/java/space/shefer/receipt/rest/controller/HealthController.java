a885350f960864921b9980977bce02cac9ee9531
Vladimir Shefer <quameu@gmail.com>
2020-05-30 05:51:56 +0300

    Specify HTTP-method for ping request

--- rest-api/src/main/java/space/shefer/receipt/rest/controller/HealthController.java
+++ rest-api/src/main/java/space/shefer/receipt/rest/controller/HealthController.java
@@ -1,12 +1,13 @@
 package space.shefer.receipt.rest.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
 
-  @RequestMapping(value = "/ping")
+  @RequestMapping(value = "/ping", method = RequestMethod.GET)
   public String ping() {
     return "pong";
   }
