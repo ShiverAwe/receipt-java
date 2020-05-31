cad7cbab59f8d5e7b2c0d80216bf17b155598598
Vladimir Shefer <quameu@gmail.com>
2020-05-31 03:34:11 +0300

    Add API Definition to HealthController

--- rest-api/src/main/java/space/shefer/receipt/rest/controller/HealthController.java
+++ rest-api/src/main/java/space/shefer/receipt/rest/controller/HealthController.java
@@ -1,5 +1,11 @@
 package space.shefer.receipt.rest.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-  @RequestMapping(value = "/ping", method = RequestMethod.GET)
+  @Operation(
+    description = "Health check method. Returns 'pong' string.",
+    responses = @ApiResponse(
+      responseCode = "200",
+      description = "Rest API is up",
+      content = @Content(schema = @Schema(
+        description = "The `pong` string"
+      ))
+    )
+  )
+  @RequestMapping(value = "/ping", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
   public String ping() {
     return "pong";
   }
+
 }
