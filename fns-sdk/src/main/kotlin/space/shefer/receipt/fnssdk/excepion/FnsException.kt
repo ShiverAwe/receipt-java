d31960797cfe791d230b10692559acdbb8b97482
vnesterov <48258305+VladNesterov@users.noreply.github.com>
2020-06-09 20:08:54 +0300

    RCJ-79 change position in param (#80)

--- fns-sdk/src/main/kotlin/space/shefer/receipt/fnssdk/excepion/FnsException.kt
+++ fns-sdk/src/main/kotlin/space/shefer/receipt/fnssdk/excepion/FnsException.kt
@@ -2,7 +2,7 @@ package space.shefer.receipt.fnssdk.excepion
 
 open class FnsException(message: String, cause: Throwable? = null) : RuntimeException(message, cause)
 
-class ReceiptNotFoundException(val fn: String, val fp: String, val fd: String)
+class ReceiptNotFoundException(val fn: String, val fd: String, val fp: String)
     : FnsException("fn=$fn, fd=$fd, fp=$fp")
 
 class AuthorizationFailedException(login: String, cause: Throwable? = null)
