7051400aeab1130de29465c149c5b64dc0f57341
Vladimir Shefer <quameu@gmail.com>
2021-01-30 19:31:57 +0300

    Create filesystem dao

---
+++ storage-filesystem/src/main/java/space/shefer/receipt/storages/Dao.java
@@ -0,0 +1,15 @@
+package space.shefer.receipt.storages;
+
+import javax.annotation.Nullable;
+import java.util.List;
+
+public interface Dao<T> {
+
+  T save(String id, T entity);
+  T getById(String id);
+  @Nullable
+  T getByIdOrNull(String id);
+  List<T> getAll();
+  void delete(String id);
+
+}
