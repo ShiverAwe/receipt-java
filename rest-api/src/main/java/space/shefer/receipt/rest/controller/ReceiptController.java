2aaf1a1a0c8d8dfe32186444ddb0a8a3bdd11c36
Vladimir Shefer <quameu@gmail.com>
2021-02-11 16:56:43 +0300

    Remove /create endpoint

--- rest-api/src/main/java/space/shefer/receipt/rest/controller/ReceiptController.java
+++ rest-api/src/main/java/space/shefer/receipt/rest/controller/ReceiptController.java
@@ -5,22 +5,11 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.lang.Nullable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import space.shefer.receipt.platform.core.entity.Receipt;
-import space.shefer.receipt.platform.core.entity.UserProfile;
-import space.shefer.receipt.platform.core.service.UserProfileService;
-import space.shefer.receipt.rest.converters.ReceiptMetaConverter;
-import space.shefer.receipt.rest.dto.ReceiptCreateDto;
-import space.shefer.receipt.rest.dto.ReceiptMetaDto;
 import space.shefer.receipt.rest.service.ReceiptService;
-
-import javax.validation.Valid;
 
 @Tag(name = "Receipts management")
 @RestController
@@ -28,24 +17,6 @@ import javax.validation.Valid;
 public class ReceiptController {
 
   private final ReceiptService receiptService;
-  private final UserProfileService userProfileService;
-
-  @Operation(
-    description = "Submit receipt for loading. If receipt is submitted " +
-      "multiple times, then deduplication could be performed.",
-    responses = @ApiResponse(responseCode = "200", description = "Receipt has been successfully created")
-  )
-  @RequestMapping(value = "/create", method = RequestMethod.POST)
-  public ReceiptMetaDto create(@Valid @RequestBody ReceiptCreateDto query,
-                               @Nullable @RequestHeader("Authorization") String authHeader) {
-    UserProfile userProfile = null;
-    if (authHeader != null) {
-      String token = authHeader.substring("Bearer ".length());
-      userProfile = userProfileService.getUserByToken(token);
-    }
-    Receipt receipt = receiptService.create(query, userProfile);
-    return ReceiptMetaConverter.toDto(receipt);
-  }
 
   @Operation(
     description = "Allows remove receipt if it is stuck in loading",
