5e41a0f5db46ccf39dbf9c5e419df96facf49aa8
Vladimir Shefer <quameu@gmail.com>
2021-02-12 18:12:47 +0300

    Add builder to ReceiptMetaDto.java

--- storage-filesystem/src/main/java/space/shefer/receipt/dto/ReceiptMetaDto.java
+++ storage-filesystem/src/main/java/space/shefer/receipt/dto/ReceiptMetaDto.java
@@ -1,5 +1,7 @@
 package space.shefer.receipt.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Builder;
 import lombok.Data;
 
 import javax.annotation.Nullable;
@@ -7,9 +9,11 @@ import javax.validation.constraints.NotNull;
 import java.time.ZonedDateTime;
 
 @Data
+@Builder
 public class ReceiptMetaDto {
 
   @NotNull
+  @JsonFormat(pattern = "yyyyMMdd'T'HHmmss")
   private ZonedDateTime dateTime;
   private int currency;
   private double sum;
