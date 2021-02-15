523eaba9dbbf8b6e2384c57ec3570ec69fb7cc7d
Vladimir Shefer <quameu@gmail.com>
2021-02-15 17:04:02 +0300

    Remove FnsSdkConfiguration.kt

--- fns-sdk/src/main/kotlin/space/shefer/receipt/fnssdk/config/FnsSdkConfiguration.kt
+++
@@ -1,8 +0,0 @@
-package space.shefer.receipt.fnssdk.config
-
-import org.springframework.context.annotation.ComponentScan
-import org.springframework.context.annotation.Configuration
-
-@ComponentScan("space.shefer.receipt.fnssdk")
-@Configuration
-class FnsSdkConfiguration
