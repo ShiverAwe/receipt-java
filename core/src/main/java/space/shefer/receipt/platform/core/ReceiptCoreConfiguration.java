45846347359e1c10261f0808bd283fd87516e544
Vladimir Shefer <quameu@gmail.com>
2021-02-25 16:55:33 +0300

    Remove unused endpoints and services

--- core/src/main/java/space/shefer/receipt/platform/core/ReceiptCoreConfiguration.java
+++ core/src/main/java/space/shefer/receipt/platform/core/ReceiptCoreConfiguration.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import space.shefer.receipt.fnssdk.config.EnableFnsSdk;
 
-@EnableFnsSdk
 @ComponentScan
 @EnableJpaRepositories
 @EnableJpaAuditing
