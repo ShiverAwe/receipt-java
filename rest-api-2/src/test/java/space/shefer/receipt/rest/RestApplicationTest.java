8ec3274ec591e75fbcebe246344ecbf1c5fc23ac
Vladimir Shefer <quameu@gmail.com>
2021-10-17 14:29:35 +0300

    Dirty commit

---
+++ rest-api-2/src/test/java/space/shefer/receipt/rest/RestApplicationTest.java
@@ -0,0 +1,14 @@
+package space.shefer.receipt.rest;
+
+import org.junit.Test;
+import space.shefer.receipt.tests.util.SpringIntegrationTest;
+
+@SpringIntegrationTest
+public class RestApplicationTest {
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
